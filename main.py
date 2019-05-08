ddff04a4dc3a23475c36afc747c7f2c5b680ab17
Aditya Saky <aditya@saky.in>
2019-05-08 14:25:46 -0400

    Add thresholding function

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import cv2
+import numpy as np
+import copy
+import os
+
+
+DATA_FOLDER = "data"
+
+
+def generate_binary_image(image, threshold):
+    image_clone = copy.deepcopy(image)
+    image_clone[image_clone > threshold] = 255
+    image_clone[image_clone <= threshold] = 0
+    return image_clone
 
 
 if __name__ == "__main__":
-    pass
+    image = cv2.cvtColor(cv2.imread(os.path.join(DATA_FOLDER, "image_3.png")), cv2.COLOR_BGR2GRAY)
+    binary_image = generate_binary_image(image, 190)
+    cv2.imshow("Image", binary_image)
+    cv2.waitKey(0)
