cc4591d9b8a3269fafe93ee1aad82182c50719ae
Aditya Saky <aditya@saky.in>
2019-05-08 19:30:36 -0400

    Add resize method

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ import os
 DATA_FOLDER = "data"
 
 
+def resize_image(image, width=400):
+    original_height, original_width = image.shape
+    height = int((original_height * 1.0 / original_width) * width)
+    return cv2.resize(image, (width, height))
+
+
 def generate_binary_image(image, threshold):
     image_clone = copy.deepcopy(image)
     image_clone[image_clone > threshold] = 255
@@ -16,7 +22,10 @@ def generate_binary_image(image, threshold):
 
 
 if __name__ == "__main__":
-    image = cv2.cvtColor(cv2.imread(os.path.join(DATA_FOLDER, "image_3.png")), cv2.COLOR_BGR2GRAY)
-    binary_image = generate_binary_image(image, 190)
+    image = cv2.cvtColor(cv2.imread(os.path.join(DATA_FOLDER, "image_4.jpeg")), cv2.COLOR_BGR2GRAY)
+
+    resized_image = resize_image(image)
+    binary_image = generate_binary_image(resized_image, 40)
+
     cv2.imshow("Image", binary_image)
     cv2.waitKey(0)
