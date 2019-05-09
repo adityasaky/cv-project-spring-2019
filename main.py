be39c98e6ba891ff24dc54b84201834ec89220c6
Pushkar Sharma <thepsguy@icloud.com>
2019-05-08 20:55:36 -0400

    get clusters and paint them red

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ DATA_FOLDER = "data"
 
 
 def resize_image(image, width=400):
-    original_height, original_width = image.shape
+    original_height, original_width, _ = image.shape
     height = int((original_height * 1.0 / original_width) * width)
     return cv2.resize(image, (width, height))
 
@@ -20,12 +20,35 @@ def generate_binary_image(image, threshold):
     image_clone[image_clone <= threshold] = 0
     return image_clone
 
+def pixel_clusters_from(binary_image):
+    previous = None
+    clusters = {}
+    current_cluster = 0
+    for p in np.argwhere(binary_image == 0):
+        if previous is None:
+            previous = p
+            clusters[current_cluster] = []
+        if abs(p[0] - previous[0]) <= 1 or abs(p[1] - previous[1]) <= 1:
+            clusters[current_cluster].append(p)
+            previous = p
+        else:
+            previous = p
+            current_cluster += 1
+            clusters[current_cluster] = [p]
+    return clusters
 
 if __name__ == "__main__":
-    image = cv2.cvtColor(cv2.imread(os.path.join(DATA_FOLDER, "image_4.jpeg")), cv2.COLOR_BGR2GRAY)
+    image = cv2.imread(os.path.join(DATA_FOLDER, "image_4.jpeg"))
 
     resized_image = resize_image(image)
-    binary_image = generate_binary_image(resized_image, 40)
+    greyscale_image = cv2.cvtColor(resized_image, cv2.COLOR_BGR2GRAY)
+    binary_image = generate_binary_image(greyscale_image, 40)
 
-    cv2.imshow("Image", binary_image)
+    pixel_clusters = pixel_clusters_from(binary_image)
+
+    for _, cluster in pixel_clusters.items():
+        for pixel in cluster:
+            resized_image[pixel[0], pixel[1]] = [0, 0, 255]
+
+    cv2.imshow("Image", resized_image)
     cv2.waitKey(0)
